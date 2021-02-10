455c193ca9525924536e02705ea717db19a037e0
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-02-10 22:16:39 +0000

    gll variant

--- builder/build-pages.py
+++ builder/build-pages.py
@@ -354,7 +354,11 @@ for file in os.listdir(element_path):
                     symfem_example += "\n\n"
                     symfem_example += f"# Create {data['name']} order {ord} on a {ref}\n"
                     symfem_example += f"element = symfem.create_element(\"{ref}\","
-                    symfem_example += f" \"{data['symfem']}\", {ord})"
+                    if "variant=" in data["symfem"]:
+                        e_name, variant = data["symfem"].split(" variant=")
+                        symfem_example += f" \"{e_name}\", {ord}, \"{variant}\")"
+                    else:
+                        symfem_example += f" \"{data['symfem']}\", {ord})"
             symfem_info += "<p class='pcode'>" + python_highlight(symfem_example) + "</p>"
             symfem_info += "</div>"
             symfem_info += "<script type='text/javascript'>\n"
@@ -400,9 +404,12 @@ for file in os.listdir(element_path):
             for e in data["examples"]:
                 cell = e.split(",")[0]
                 order = int(e.split(",")[1])
-                element_type = data["symfem"]
-
-                element = create_element(cell, element_type, order)
+                if "variant=" in data["symfem"]:
+                    element_type, variant = data["symfem"].split(" variant=")
+                    element = create_element(cell, element_type, order, variant)
+                else:
+                    element_type = data["symfem"]
+                    element = create_element(cell, element_type, order)
 
                 eg = markup_element(element)
 
