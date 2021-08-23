356f6fa548ea54065a89888a8733646e65bce716
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-08-23 10:34:01 +0100

    Add reference to Cockburn-Foo and Arnold-Logg on element pages (#128)
    * Add reference to Cockburn-Foo and Arnold-Logg (fixes #126)
    
    * minimum numba version
    
    * limit numpy version for now
    
    * = nto <=
    
    * ==

--- builder/families.py
+++ builder/families.py
@@ -29,3 +29,26 @@ def cockburn_foo_name(family, r="r", cell=None, degree="k"):
     if r != "r":
         out += f"\\right]_{{{r}}}"
     return out
+
+
+arnold_logg_reference = {
+    "title": "Periodic table of the finite elements",
+    "author": "Arnold, D. and Logg, A.",
+    "journal": "SIAM News",
+    "year": "2014",
+    "volume": "47",
+    "number": "9",
+    "url": "https://sinews.siam.org/Details-Page/periodic-table-of-the-finite-elements"
+}
+
+cockburn_foo_reference = {
+    "title": "A systematic construction of finite element commuting exact sequences",
+    "author": "Cockburn, B. and Foo, G.",
+    "journal": "SIAM journal on numerical analysis",
+    "volume": "55",
+    "number": "4",
+    "pagestart": "1650",
+    "pageend": "1688",
+    "year": "2017",
+    "doi": "10.1137/16M1073352"
+}
