1ee7778f870e045f402420c746a1acd370060410
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-14 14:36:32 +0100

    remove info that's moved to symfem

--- builder/examples.py
+++ builder/examples.py
@@ -1,7 +1,6 @@
 from . import symbols
 from . import plotting
 import sympy
-from symfem import functionals
 from symfem.symbolic import PiecewiseFunction, t
 from symfem.finite_element import CiarletElement, DirectElement
 
@@ -35,306 +34,27 @@ def to_tex(f, tfrac=False):
         return out
 
 
-def get_entity_number(element, dof):
-    if dof.entity_dim() == 1:
-        entities = element.reference.edges
-    elif dof.entity_dim() == 2:
-        entities = element.reference.faces
-    elif dof.entity_dim() == 3:
-        entities = element.reference.volumes
-    return str(entities.index(
-        tuple(element.reference.vertices.index(i)
-              for i in dof.reference.vertices)))
-
-
 def entity_name(dim):
     return ["vertex", "edge", "face", "volume"][dim]
 
 
-def nth(n):
-    if n % 10 == 1 and n != 11:
-        return f"{n}st"
-    if n % 10 == 2 and n != 12:
-        return f"{n}nd"
-    return f"{n}th"
-
-
-def define_symbol(symbol):
-    for dim in range(4):
-        for i in range(12):
-            if symbol == f"{symbols.entity(dim)}_{{{i}}}":
-                return f"\\({symbol}\\) is the {nth(i)} {entity_name(dim)}"
-
-    for i in range(12):
-        if symbol == f"\\hat{{\\boldsymbol{{n}}}}_{{{i}}}":
-            return f"\\({symbol}\\) is the normal to facet {i}"
-
-    for i in range(12):
-        if symbol == f"\\hat{{\\boldsymbol{{t}}}}_{{{i}}}":
-            return f"\\({symbol}\\) is the tangent to edge {i}"
-
-    if symbol == "R":
-        return f"\\({symbol}\\) is the reference element"
-
-    if symbol[:5] == "param":
-        for dim in range(4):
-            for i in range(12):
-                if symbol[5:] in [f"{symbols.entity(dim)}_{{{i}}}", f"R({dim})"]:
-                    if symbol[5] == f"R({dim})":
-                        ref = "R"
-                    else:
-                        ref = symbol[5:]
-                    assert dim > 0
-                    if dim == 1:
-                        return (f"\\({defelement_t[0]}\\)"
-                                f" is a parametrisation of \\({ref}\\)")
-                    else:
-                        return (f"\\(({','.join(defelement_t[:dim])})\\)"
-                                f" is a parametrisation of \\({ref}\\)")
-
-    raise ValueError(f"Unrecognised symbol: {symbol}")
-
-
 def describe_dof(element, d):
-    desc, symb = _describe_dof(element, d)
-    for i in defelement_t:
-        if i in desc:
-            if symb[0] == "R":
-                symb.append(f"paramR({element.reference.tdim})")
+    desc, symb = d.get_tex()
+
+    for i, j in zip(t, defelement_t):
+        desc = desc.replace(sympy.latex(i), j)
+
+        if j in desc:
+            dim = element.reference.tdim
+            new_s = "\\("
+            if dim == 1:
+                new_s += defelement_t[0]
             else:
-                symb.append("param" + symb[0])
+                new_s += ','.join(defelement_t[:dim])
+            new_s += f"\\) is a parametrisation of \\({symb[0]}\\)"
             break
+
     return desc, symb
-
-
-def _is_exact_instance(object, _class):
-    return type(object) == _class
-
-
-def _describe_dof(element, d):
-    if _is_exact_instance(d, functionals.PointEvaluation):
-        return "v\\mapsto v(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")", []
-    elif _is_exact_instance(d, functionals.DotPointEvaluation):
-        desc = "\\boldsymbol{v}\\mapsto"
-        desc += "\\boldsymbol{v}(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-        desc += "\\cdot\\left(\\begin{array}{c}"
-        desc += "\\\\".join([to_tex(i) for i in d.dof_direction()])
-        desc += "\\end{array}\\right)"
-        return desc, []
-    if _is_exact_instance(d, functionals.WeightedPointEvaluation):
-        desc = "v\\mapsto " + to_tex(d.weight)
-        desc += " v(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-        return desc, []
-    elif _is_exact_instance(d, functionals.PointNormalDerivativeEvaluation):
-        desc = "v\\mapsto"
-        desc += "\\nabla{v}(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-        entity_n = get_entity_number(element, d)
-        desc += "\\cdot\\hat{\\boldsymbol{n}}" + f"_{{{entity_n}}}"
-        return desc, ["\\hat{\\boldsymbol{n}}" + f"_{{{entity_n}}}"]
-    elif _is_exact_instance(d, functionals.PointDirectionalDerivativeEvaluation):
-        if element.reference.tdim == 1:
-            desc = "v\\mapsto "
-            desc += "v'(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-            return desc, []
-        desc = "v\\mapsto"
-        desc += "\\nabla{v}(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-        desc += "\\cdot\\left(\\begin{array}{c}"
-        desc += "\\\\".join([to_tex(i) for i in d.dof_direction()])
-        desc += "\\end{array}\\right)"
-        return desc, []
-    elif _is_exact_instance(d, functionals.DerivativePointEvaluation):
-        if element.reference.tdim == 1:
-            desc = "v\\mapsto "
-            desc += "v'(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-            return desc, []
-        desc = "v\\mapsto"
-        desc += "\\frac{\\partial"
-        if sum(d.derivative) > 1:
-            desc += f"^{{{sum(d.derivative)}}}"
-        desc += "}{"
-        for v, i in zip("xyz", d.derivative):
-            if i > 0:
-                desc += f"\\partial {v}"
-                if i > 1:
-                    desc += f"^{{{i}}}"
-        desc += "}"
-        desc += "v(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-        return desc, []
-    elif _is_exact_instance(d, functionals.PointComponentSecondDerivativeEvaluation):
-        desc = "v\\mapsto"
-        desc += "\\frac{\\partial^2v}{"
-        for c in d.component:
-            desc += "\\partial " + "xyz"[c]
-        desc += "}(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-        return desc, []
-    elif _is_exact_instance(d, functionals.TangentIntegralMoment):
-        entity = get_entity(element, d)
-        entity_n = get_entity_number(element, d)
-        desc = "\\boldsymbol{v}\\mapsto"
-        desc += f"\\displaystyle\\int_{{{entity}}}"
-        desc += "\\boldsymbol{v}\\cdot"
-        if d.f != 1:
-            desc += "(" + to_tex(d.f) + ")"
-        desc += "\\hat{\\boldsymbol{t}}" + f"_{{{entity_n}}}"
-        return desc, [entity, "\\hat{\\boldsymbol{t}}" + f"_{{{entity_n}}}"]
-    elif _is_exact_instance(d, functionals.NormalIntegralMoment):
-        entity = get_entity(element, d)
-        entity_n = get_entity_number(element, d)
-        desc = "\\boldsymbol{v}\\mapsto"
-        desc += f"\\displaystyle\\int_{{{entity}}}"
-        desc += "\\boldsymbol{v}\\cdot"
-        if d.f != 1:
-            desc += "(" + to_tex(d.f, True) + ")"
-        desc += "\\hat{\\boldsymbol{n}}" + f"_{{{entity_n}}}"
-        return desc, [entity, "\\hat{\\boldsymbol{n}}" + f"_{{{entity_n}}}"]
-    elif _is_exact_instance(d, functionals.NormalDerivativeIntegralMoment):
-        entity = get_entity(element, d)
-        entity_n = get_entity_number(element, d)
-        desc = "v\\mapsto"
-        desc += f"\\displaystyle\\int_{{{entity}}}"
-        if d.f != 1:
-            desc += "(" + to_tex(d.f, True) + ")"
-        desc += "\\frac{\\partial v}"
-        desc += "{\\partial\\hat{\\boldsymbol{n}}" + f"_{{{entity_n}}}" + "}"
-        return desc, [entity, "\\hat{\\boldsymbol{n}}" + f"_{{{entity_n}}}"]
-    elif _is_exact_instance(d, functionals.DivergenceIntegralMoment):
-        entity = get_entity(element, d)
-        entity_n = get_entity_number(element, d)
-        desc = "\\boldsymbol{v}\\mapsto"
-        desc += f"\\displaystyle\\int_{{{entity}}}"
-        if d.f != 1:
-            desc += "(" + to_tex(d.f, True) + ")"
-        desc += "\\nabla\\cdot\\boldsymbol{v}"
-        return desc, [entity]
-    elif _is_exact_instance(d, functionals.InnerProductIntegralMoment):
-        entity = get_entity(element, d)
-        entity_n = get_entity_number(element, d)
-        desc = "\\boldsymbol{V}\\mapsto"
-        desc += f"\\displaystyle\\int_{{{entity}}}"
-        if d.f != 1:
-            desc += "(" + to_tex(d.f, True) + ")"
-        desc += to_tex(d.inner_with_left) + symbols.transpose
-        desc += "\\boldsymbol{V}"
-        desc += to_tex(d.inner_with_right)
-        return desc, [entity]
-    elif _is_exact_instance(d, functionals.NormalInnerProductIntegralMoment):
-        entity = get_entity(element, d)
-        entity_n = get_entity_number(element, d)
-        desc = "\\mathbf{V}\\mapsto"
-        desc += f"\\displaystyle\\int_{{{entity}}}"
-        if d.f != 1:
-            desc += "(" + to_tex(d.f, True) + ")"
-        desc += f"|{{{entity}}}|"
-        desc += "\\hat{\\boldsymbol{n}}" + symbols.transpose + f"_{{{entity_n}}}"
-        desc += "\\mathbf{V}"
-        desc += "\\hat{\\boldsymbol{n}}" + f"_{{{entity_n}}}"
-        return desc, [entity, "\\hat{\\boldsymbol{n}}" + f"_{{{entity_n}}}"]
-    elif _is_exact_instance(d, functionals.IntegralAgainst):
-        if element.range_dim == 1:
-            entity = get_entity(element, d)
-            entity_n = get_entity_number(element, d)
-            desc = "\\mathbf{v}\\mapsto"
-            desc += f"\\displaystyle\\int_{{{entity}}}"
-            if d.f != 1:
-                desc += "(" + to_tex(d.f, True) + ")"
-            desc += "v"
-            return desc, [entity]
-        else:
-            assert element.range_shape is None
-            entity = get_entity(element, d)
-            entity_n = get_entity_number(element, d)
-            desc = "\\mathbf{v}\\mapsto"
-            desc += f"\\displaystyle\\int_{{{entity}}}"
-            desc += to_tex(d.f, True) + "\\cdot"
-            desc += "\\mathbf{v}"
-            return desc, [entity]
-    elif _is_exact_instance(d, functionals.IntegralOfDivergenceAgainst):
-        assert element.range_dim == element.reference.tdim
-        entity = get_entity(element, d)
-        entity_n = get_entity_number(element, d)
-        desc = "\\mathbf{v}\\mapsto"
-        desc += f"\\displaystyle\\int_{{{entity}}}"
-        if d.f != 1:
-            desc += "(" + to_tex(d.f, True) + ")"
-        desc += "\\nabla\\cdot\\mathbf{v}"
-        return desc, [entity]
-    elif _is_exact_instance(d, functionals.IntegralOfDirectionalMultiderivative):
-        entity = get_entity(element, d)
-        entity_n = get_entity_number(element, d)
-        desc = "\\mathbf{V}\\mapsto"
-        desc += "\\displaystyle"
-        if d.scale != 1:
-            desc += to_tex(d.scale)
-        desc += f"\\int_{{{entity}}}"
-        for order, dir in zip(d.orders, d.directions):
-            if order > 0:
-                desc += "\\frac{\\partial"
-                if order > 1:
-                    desc += f"^{{{order}}}"
-                desc += "}{"
-                desc += "\\partial" + to_tex(dir)
-                if order > 1:
-                    desc += f"^{{{order}}}"
-                desc += "}"
-        desc += "v"
-        return desc, [entity]
-    elif _is_exact_instance(d, functionals.VecIntegralMoment):
-        entity = get_entity(element, d)
-        desc = "\\boldsymbol{v}\\mapsto"
-        desc += f"\\displaystyle\\int_{{{entity}}}"
-        if d.f != 1:
-            desc += "(" + to_tex(d.f, True) + ")"
-        desc += "\\boldsymbol{v}\\cdot"
-        desc += to_tex(d.dot_with)
-        return desc, [entity]
-    elif _is_exact_instance(d, functionals.IntegralMoment):
-        entity = get_entity(element, d)
-        try:
-            d.f[0]
-            if len(d.f) == element.reference.tdim:
-                desc = "\\boldsymbol{v}\\mapsto"
-                desc += f"\\displaystyle\\int_{{{entity}}}"
-                desc += "\\boldsymbol{v}\\cdot"
-                desc += "\\left(\\begin{array}{c}"
-                desc += "\\\\".join([to_tex(i) for i in d.f])
-                desc += "\\end{array}\\right)"
-            else:
-                assert len(d.f) == element.reference.tdim ** 2
-                desc = "\\mathbf{V}\\mapsto"
-                desc += f"\\displaystyle\\int_{{{entity}}}"
-                desc += "\\mathbf{V}:"
-                desc += "\\left(\\begin{array}{" + "c" * element.reference.tdim + "}"
-                desc += "\\\\".join(["&".join(
-                    [to_tex(d.f[i]) for i in range(element.reference.tdim * row,
-                                                   element.reference.tdim * (row + 1))]
-                ) for row in range(element.reference.tdim)])
-                desc += "\\end{array}\\right)"
-        except:  # noqa: E722
-            desc = "v\\mapsto"
-            desc += f"\\displaystyle\\int_{{{entity}}}"
-            if d.f != 1:
-                desc += "(" + to_tex(d.f) + ")"
-            desc += "v"
-        return desc, [f"{entity}"]
-    elif _is_exact_instance(d, functionals.PointInnerProduct):
-        desc = "\\mathbf{V}\\mapsto"
-        desc += "\\left(\\begin{array}{c}"
-        desc += "\\\\".join([to_tex(i) for i in d.lvec])
-        desc += "\\end{array}\\right)" + symbols.transpose
-        desc += "\\mathbf{V}(" + ",".join([to_tex(i, True) for i in d.dof_point()]) + ")"
-        desc += "\\left(\\begin{array}{c}"
-        desc += "\\\\".join([to_tex(i) for i in d.rvec])
-        desc += "\\end{array}\\right)"
-        return desc, []
-    else:
-        raise ValueError(f"Unknown functional: {d.__class__}")
-
-
-def get_entity(element, d):
-    if d.entity_dim() == element.reference.tdim:
-        return symbols.reference
-    else:
-        return f"{symbols.entity(d.entity_dim())}_{{{get_entity_number(element, d)}}}"
 
 
 def markup_example(element):
@@ -372,10 +92,8 @@ def markup_example(element):
             dof = element.dofs[dof_i]
             eg += f"\\(\\displaystyle {symbols.functional}_{{{dof_i}}}:"
             dof_tex, symbols_used = describe_dof(element, dof)
-            symbols_used += []
             eg += dof_tex + "\\)"
             if len(symbols_used) > 0:
-                symbols_used = [define_symbol(i) for i in symbols_used]
                 eg += "<br />where " + ";<br />".join(symbols_used[:-1])
                 if len(symbols_used) > 1:
                     eg += ";<br />and "
