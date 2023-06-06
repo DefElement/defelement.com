8b5356cd12e59ffb461ec9d27cce9fdb8dd4bacc
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-06-06 10:09:17 +0100

    Add sitemap

--- builder/tools.py
+++ builder/tools.py
@@ -1,15 +1,25 @@
 import yaml
+from . import settings
+from .markup import preprocess
 
 
 def parse_metadata(content):
-    metadata = {}
+    metadata = {"title": None}
     if content.startswith("--\n"):
-        metadata, content = content[3:].split("\n--\n", 1)
-        metadata = yaml.load(metadata, Loader=yaml.FullLoader)
-    return metadata, content.strip()
+        metadata_in, content = content[3:].split("\n--\n", 1)
+        metadata.update(yaml.load(metadata_in, Loader=yaml.FullLoader))
+    content = preprocess(content.strip())
+    if metadata["title"] is None and content.startswith("# "):
+        metadata["title"] = content[2:].split("\n", 1)[0].strip()
+    return metadata, content
 
 
 def insert_author_info(content, authors, url):
     assert content.startswith("# ")
     title, content = content.split("\n", 1)
     return f"{title}\n{{{{author-info::{';'.join(authors)}|{title[1:].strip()}|{url}}}}}\n{content}"
+
+
+def html_local(path):
+    assert path.startswith(settings.html_path)
+    return path[len(settings.html_path):]
