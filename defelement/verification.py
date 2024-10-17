e055653d75b0c5c073e49803058d8a92016d1b6d
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-10-17 12:06:34 +0100

    Improve verification (#355)
    * get_space_dimension -> space_dimension
    
    * add bernardi-raugel to verification testing
    
    * fix bug in verification
    
    * wasn't a bug, so undo. Add --impl flag to verification
    
    * print reasons after rather than before
    
    * update test

--- defelement/verification.py
+++ defelement/verification.py
@@ -118,14 +118,14 @@ def same_span(table0: Array, table1: Array, complete: bool = True) -> bool:
         return False
 
     ndofs = table0.shape[-1]
-    table0 = table0.reshape(-1, ndofs).T
-    table1 = table1.reshape(-1, ndofs).T
+    table0 = table0.reshape(-1, ndofs)
+    table1 = table1.reshape(-1, ndofs)
 
     rank = np.linalg.matrix_rank(table0)
     if complete and rank != ndofs:
         return False
 
-    stack = np.vstack([table0, table1])
+    stack = np.hstack([table0, table1])
     srank = np.linalg.matrix_rank(stack)
     return rank == srank
 
@@ -136,18 +136,16 @@ def verify(
                         typing.Callable[[Array], Array]],
     info1: typing.Tuple[typing.List[typing.List[typing.List[int]]],
                         typing.Callable[[Array], Array]],
-    printing: bool = False
-) -> bool:
+) -> typing.Tuple[bool, typing.Optional[str]]:
     """Run verification.
 
     Args:
         ref: Reference cell
         info0: Verification info for first implementation
         info1: Verification info for second implementation
-        printing: Toggle printing
 
     Returns:
-        True if verification successful, otherwise False
+        (True, None) if verification successful, otherwise False plus a reason
     """
     import numpy as np
 
@@ -159,22 +157,16 @@ def verify(
 
     # Check the same number of entity DOFs
     if len(edofs0) != len(edofs1):
-        if printing:
-            print(f"  Wrong number of entities ({len(edofs0)} vs {len(edofs1)})")
-        return False
+        return False, f"Wrong number of entities ({len(edofs0)} vs {len(edofs1)})"
     entity_counts = []
     for dim, (i0, i1) in enumerate(zip(edofs0, edofs1)):
         if len(i0) != len(i1):
-            if printing:
-                print(f"  Wrong number of entities of dim {dim} ({len(i0)} vs {len(i1)})")
-            return False
+            return False, f"Wrong number of entities of dim {dim} ({len(i0)} vs {len(i1)})"
         entity_counts.append(len(i0))
         for e_n, (j0, j1) in enumerate(zip(i0, i1)):
             if len(j0) != len(j1):
-                if printing:
-                    print("  Wrong number of DOFs associated with an entity"
-                          f" {dim},{e_n} ({len(j0)} vs {len(j1)})")
-                return False
+                return False, ("Wrong number of DOFs associated with an entity"
+                               f" {dim},{e_n} ({len(j0)} vs {len(j1)})")
 
     # Check that polysets span the same space
     pts = points(ref)
@@ -182,14 +174,10 @@ def verify(
     table1 = tab1(pts)
 
     if table0.shape != table1.shape:
-        if printing:
-            print(f"  Non-matching table shapes ({table0.shape} vs {table1.shape})")
-        return False
+        return False, f"Non-matching table shapes ({table0.shape} vs {table1.shape})"
 
     if not same_span(table0, table1):
-        if printing:
-            print("  Polysets do not span the same space")
-        return False
+        return False, "Polysets do not span the same space"
 
     # Check that continuity will be the same
     epoints = entity_points(ref)
@@ -199,13 +187,11 @@ def verify(
             if len(ed0) > 0:
                 ed1 = ecdofs1[d][e]
 
-                not_ed0 = [k for i in ecdofs0 for j in i for k in j if k not in ed0]
-                not_ed1 = [k for i in ecdofs1 for j in i for k in j if k not in ed1]
+                not_ed0 = [k for i in edofs0 for j in i for k in j if k not in ed0]
+                not_ed1 = [k for i in edofs1 for j in i for k in j if k not in ed1]
                 t0 = tab0(pts)[:, :, not_ed0]
                 t1 = tab1(pts)[:, :, not_ed1]
                 if not np.allclose(t0, t1) and not same_span(t0, t1, False):
-                    if printing:
-                        print(f"  Continuity does not match for ({d},{e})")
-                    return False
+                    return False, f"Continuity does not match for ({d},{e})"
 
-    return True
+    return True, None
--- verify.py
+++ verify.py
@@ -24,6 +24,8 @@ parser.add_argument('--skip-missing-libraries', default="true",
                     help="Skip verification if library is not installed.")
 parser.add_argument('--print-reasons', default="false",
                     help="Show reasons for failed verification")
+parser.add_argument('--impl', metavar="impl", default=None,
+                    help="libraries to run verification for")
 
 args = parser.parse_args()
 if args.destination is not None:
@@ -40,6 +42,10 @@ elif args.test == "auto":
         "bernardi-raugel"]
 else:
     test_elements = args.test.split(",")
+if args.impl is None:
+    test_implementations = None
+else:
+    test_implementations = args.impl.split(",")
 skip_missing = args.skip_missing_libraries == "true"
 print_reasons = args.print_reasons == "true"
 
@@ -54,7 +60,11 @@ elements_to_verify = []
 for e in categoriser.elements:
     if test_elements is None or e.filename in test_elements:
         for eg in e.examples:
-            implementations = [i for i in verifications if i != "symfem" and e.implemented(i)]
+            implementations = [
+                i for i in verifications
+                if i != "symfem" and e.implemented(i) and (
+                    test_implementations is None or i in test_implementations)
+            ]
             if len(implementations) > 0:
                 elements_to_verify.append((e, eg, implementations))
 
@@ -92,12 +102,15 @@ def verify_examples(
         for i in implementations:
             try:
                 vinfo = verifications[i](e, eg)
-                if verify(cell, vinfo, sym_info, print_reasons):
+                v, info = verify(cell, vinfo, sym_info)
+                if v:
                     results[e.filename][i]["pass"].append(eg)
                     print(f"{process}{e.filename} {i} {eg} {green}\u2713{default}")
                 else:
                     results[e.filename][i]["fail"].append(eg)
                     print(f"{process}{e.filename} {i} {eg} {red}\u2715{default}")
+                    if print_reasons:
+                        print(f"  {info}")
             except ImportError as err:
                 if skip_missing:
                     print(f"{process}{i} not installed")
