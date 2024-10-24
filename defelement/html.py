b5985f6256c0cece667fd80d704421934e7dcbd0
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-10-24 16:59:55 +0100

    Fix #354 (#363)

--- defelement/html.py
+++ defelement/html.py
@@ -22,8 +22,10 @@ def make_html_page(content: str, pagetitle: typing.Optional[str] = None) -> str:
         out += insert_dates(f.read())
     if pagetitle is None:
         out = out.replace("{{: pagetitle}}", "")
+        out = out.replace("{{pagetitle |}}", "")
     else:
         out = out.replace("{{: pagetitle}}", f": {pagetitle}")
+        out = out.replace("{{pagetitle |}}", f"{pagetitle} |")
     out += content
     with open(os.path.join(settings.template_path, "outro.html")) as f:
         out += insert_dates(f.read())
